a2e72b35ab58910712d9447791b48ddcc20a2273
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2022-06-06 15:06:09 +0200

    Update 01-inc.cpp

--- sources/01-inc.cpp
+++ sources/01-inc.cpp
@@ -14,5 +14,21 @@ int main(int argc, const char** argv) {
 
     int b = 0;
     std::cout << ++b << std::endl;
+    
+    // Example with the sum
+    int c = 5;
+    std::cout << c << std::endl; // print 5
+    c = 5;
+    std::cout << c++ << std::endl; // print 5
+    c = 5;
+    std::cout << ++c << std::endl; // print 6
+    c = 5;
+    std::cout << c++ + c++ << std::endl; // print 11
+    c = 5;
+    std::cout << c++ + ++c << std::endl; // print 12
+    c = 5;
+    std::cout << ++c + c++ << std::endl; // print 13
+    c = 5;
+    std::cout << ++c + ++c << std::endl; // print 14
     return EXIT_SUCCESS;
 }
